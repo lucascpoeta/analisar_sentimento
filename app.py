4da09e808914456420df09be2b1a4cdd4d291739
Lucas Gil de Oliveira Carmona Poeta <lucas.poeta@iesb.edu.br>
2025-04-15 00:26:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
 
-nlp = spacy.load("en_core_web_md")
+import en_core_web_md
+nlp = en_core_web_md.load()
 
 
 
